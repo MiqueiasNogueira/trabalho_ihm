9383eb197822585e2ce1fbeecbec2588295c1177
Miqueias Nogueira Santos <miqueiasnogueirasantos567@gmail.com>
2025-11-17 14:32:05 -0300

    deploy: Versão de produção gerada na pasta docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const repoName = 'trabalho_ihm'; 
+
 export default defineConfig({
   plugins: [react()],
-})
+
+  base: `/${repoName}/`, 
+  build: {
+    outDir: 'docs' // Opcional, mas comum em Pages
+  }
+});
