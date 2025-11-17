8e17ecdcb3bf43d4327abbe6b635c160b2201511
Miqueias Nogueira Santos <miqueiasnogueirasantos567@gmail.com>
2025-11-17 14:43:24 -0300

    fix: Configurando as pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 const repoName = 'trabalho_ihm'; 
 
 export default defineConfig({
   plugins: [react()],
+  
 
   base: `/${repoName}/`, 
+  
   build: {
-    outDir: 'docs' // Opcional, mas comum em Pages
+
+    outDir: 'docs' 
   }
 });
