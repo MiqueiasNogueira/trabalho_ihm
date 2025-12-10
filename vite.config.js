4042ff48524defcb59a1cd22b3b5c99d5ecd97e5
Miqueias Nogueira Santos <miqueiasnogueirasantos567@gmail.com>
2025-12-09 22:55:34 -0300

    Correção de rotas para o git pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path'
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, '..');
+
 
 export default defineConfig({
   plugins: [react()],
   base: "/trabalho_ihm/",
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "index.html"),
+      },
+    },
   }
 });
